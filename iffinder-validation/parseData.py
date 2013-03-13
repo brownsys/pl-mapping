a26d23d0509ddfde5652ce58a4841760e5db1dc2
Jordan Place <jplace@cs.brown.edu>
2013-03-13 12:57:32 -0400

    fixed iffinder verification algorithm

--- iffinder-validation/parseData.py
+++ iffinder-validation/parseData.py
@@ -11,11 +11,6 @@ to interfaces on the same router.
 import sys
 import re
 
-# statistics
-numParsed = 0
-numUniqueAlias = 0
-avgInterfaces = 0
-
 # parse the file into usable tuples
 parsed = []
 for line in sys.stdin.readlines():
@@ -36,35 +31,95 @@ for line in sys.stdin.readlines():
 	else:
 		continue
 
-# build a dictionary of alias IPs { alias IP -> [ interface IPs ] }
-aliases = {}
+# next router ID to give out
+nextRouterID = 0
+# { router ID -> set(interface IPs) }
+routerToIPs = {}
+# { interface IP -> router ID }
+ipToRouter = {}
+
 
 # load candidates based on aliases responses
 for entry in parsed:
 	addr = entry[0]
 	alias = entry[1]
-	if alias in aliases:
-		aliases[alias].append(addr)
-	else:
-		aliases[alias] = [addr]
+	result = entry[4]
 
-#TODO: Compensate for "results" field
+	# only use this parsed entry if it successfully elicited a response
+	if result != "D" and result != "S":
+		continue
+
+	# we've seen both these IPs before
+	if addr in ipToRouter and alias in ipToRouter:
+		addrRouter = ipToRouter[addr]
+		aliasRouter = ipToRouter[alias]
+		# if we don't think these two IPs are on the same router
+		# then join them into one router
+		if addrRouter != aliasRouter:
+			# we'll use the addr router as the combined router ID
+			# update the routerToIPs mapping
+			routerToIPs[addrRouter] = routerToIPs[addrRouter].union(routerToIPs[aliasRouter])
+			# update the ipToRouter mapping
+			for ip in routerToIPs[aliasRouter]:
+				ipToRouter[ip] = addrRouter
+			# delete old router
+			del routerToIPs[aliasRouter]
+		# we already knew these were on the same router
+		else:
+			continue
+	# we've seen the addr IP before but not alias IP
+	elif addr in ipToRouter:	
+		addrRouter = ipToRouter[addr]
+		# update the routerToIPs mapping
+		routerToIPs[addrRouter].add(alias)
+		# update the ipToRouter mapping
+		ipToRouter[alias] = addrRouter
+	# we've seen the alias IP before but not addr IP
+	elif alias in ipToRouter:
+		aliasRouter = ipToRouter[alias]
+		# update the routerToIPs mapping
+		routerToIPs[aliasRouter].add(addr)
+		# update the ipToRouter mapping
+		ipToRouter[addr] = aliasRouter
+	# wev've never seen either IP before
+	else:
+		# get new router ID
+		newRouter = nextRouterID
+		nextRouterID += 1
+		# update the routerToIPs mapping
+		routerToIPs[newRouter] = set([addr, alias])
+		# update the ipToRouter mapping
+		ipToRouter[addr] = newRouter
+		ipToRouter[alias] = newRouter
+
 
 #TODO: What additional sanity checks do we need?
-#	-Crunch data in graphs to reduce aliasing data
 #	-Reverse DNS record similarities
 
 #TODO: Graph edge analysis?
 #	-/30 blocks should be connected by a link
 #	-Traceroute shows connections
 
+# sanity check results by ensuring every IP belongs to exactly one router
+allIPs = set()
+for router in routerToIPs:
+	for ip in routerToIPs[router]:
+		if ip in allIPs:
+			sys.stderr.write("IP " + ip + " already found!\n")
+		else:
+			allIPs.add(ip)
 
 
+# print statistic to stderr
 numParsed = len(parsed)
-numUniqueAlias = len(aliases.keys())
-lengthsOfAliases = map(len, aliases.values())
-avgInterfaces = sum(lengthsOfAliases)/len(aliases.keys())
-
+numRouters = len(routerToIPs.keys())
+numInterfaces = map(len, routerToIPs.values())
+avgInterfaces = sum(numInterfaces)/numRouters
 sys.stderr.write("Number of lines parsed: " + str(numParsed) + "\n")
-sys.stderr.write("Number of unique aliases found: " + str(numUniqueAlias) + " (" + str(int((numUniqueAlias / (1.0 * numParsed)) * 100)) +"% of parsed)\n")
-sys.stderr.write("Number of addrs to aliases:\n\tAvg: " + str(avgInterfaces) + "\n\tMax: " + str(max(lengthsOfAliases)) + "\n\tMin: " + str(min(lengthsOfAliases)) + "\n")
+sys.stderr.write("Number of routers found: " + str(numRouters) + " (" + str(int((numRouters / (1.0 * numParsed)) * 100)) +"% of parsed)\n")
+sys.stderr.write("Number of addrs to aliases:\n\tAvg: " + str(avgInterfaces) + "\n\tMax: " + str(max(numInterfaces)) + "\n\tMin: " + str(min(numInterfaces)) + "\n")
+
+# print router information to stdout
+for router in routerToIPs:
+	print "Router " + str(router) + " -> " + str(sorted(routerToIPs[router]))
+
