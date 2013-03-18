1851756138b2e167eb9ca4f5ba3078c3237a66a9
DTrejo <david.daniel.trejo@gmail.com>
2013-03-18 16:16:40 -0400

    code to turn atlas-faa.txt into a json of lat long info

--- airport-codes/airports.js
+++ airport-codes/airports.js
@@ -2,6 +2,7 @@ var fs = require('fs')
 var csv = require('csv')
 var _ = require('underscore')
 
+// these are the airport codes seen in DNS data
 var cogentcodes =
   fs.readFileSync('./atlas-faa.txt', 'utf8')
   .split('\n')
@@ -12,8 +13,7 @@ var cogentcodes =
 
 var allcodes = []
 
-csv()
-.from(fs.readFileSync('./airports.csv', 'utf8'))
+csv().from(fs.readFileSync('./airports.csv', 'utf8'))
 
 // 0 Airport ID  Unique OpenFlights identifier for this airport.
 // 1 Name        Name of airport. May or may not contain the City name.
@@ -31,12 +31,21 @@ csv()
   var faa = row[4].trim()
   var icao = row[5].trim().replace('\\N', '')
   var code = faa || icao;
-  console.log(code || '---', name)
+  // console.log(code || '---', name)
   if (code) allcodes.push(code.toLowerCase())
 
 }).on('end', function(count) {
-  console.log('\nCogent codes: ' + allcodes.join(' '))
-  console.log('Airport codes: ' + cogentcodes.join(' '))
-  console.log('\nCogent codes not found in the airports.csv:')
-  console.log(_.difference(cogentcodes, allcodes).join(' '))
+  // console.log('All the airport codes: ' + allcodes.join(' '))
+
+  // console.log('Cogent\'s codes: ' + cogentcodes.join(' '))
+
+  fs.writeFileSync('./output-cogent-codes.txt'
+    , cogentcodes.join('\n'))
+
+  // console.log('\nCogent codes not found in the airports.csv:')
+  fs.writeFileSync('./output-codes-not-in-airports-csv.txt'
+    , _.difference(cogentcodes, allcodes).join('\n'))
+
+  console.log('Done. See', './output-cogent-codes.txt', 'and'
+    , './output-codes-not-in-airports-csv.txt')
 })
