6d143967fd40faa686d548050abd969acf706a6a
Andrew Ferguson <adf@cs.brown.edu>
2013-03-21 16:56:55 -0400

    Improve some of the processing rules

--- master/pl_bin/collect-pairs.py
+++ master/pl_bin/collect-pairs.py
@@ -4,24 +4,29 @@ import sys
 
 ipIndex = {}
 
+# Creates a dictionary from IP address (in decimal) to
+#     {ip addr, interface type, location, router in that location}
 for line in sys.stdin.readlines():
-	pieces = line.split()
-	model = pieces[-1]
-	ip_txt = pieces[-2]
-	loc	= pieces[0]
-	router = pieces[1]
+    pieces = line.split()
+    model = pieces[-1]
+    ip_txt = pieces[-2]
+    loc    = pieces[0]
+    router = pieces[1]
 
-	ip_parts = ip_txt.split(".")
-	ip = int(ip_parts[0]) * 256 * 256 * 256 + \
-			int(ip_parts[1]) * 256 * 256 + int(ip_parts[2]) * 256 + \
-			int(ip_parts[3])
+    ip_parts = ip_txt.split(".")
+    ip = int(ip_parts[0]) * 256 * 256 * 256 + \
+            int(ip_parts[1]) * 256 * 256 + int(ip_parts[2]) * 256 + \
+            int(ip_parts[3])
 
-	ipIndex[ip] = (ip_txt, model, loc, router)
+    ipIndex[ip] = (ip_txt, model, loc, router)
 
+# Hunts for /30's where we have both usable IPs in our list of routers
 last_val = -1
 for val in sorted(ipIndex):
-	if (val - last_val) == 1:
-		print("%s\t%s\t%s\t%s\t" % ipIndex[last_val]),
-		print("%s\t%s\t%s\t%s" % ipIndex[val])
-#		print "%s %s" % (ipIndex[last_val], ipIndex[val])
-	last_val = val
+    if (val - last_val) == 1 and \
+            ((val + 1) not in ipIndex) and \
+            ((val - 2) not in ipIndex) and \
+            ((val - 2) % 4 == 0):
+        print("%s\t%s\t%s\t%s\t" % ipIndex[last_val]),
+        print("%s\t%s\t%s\t%s" % ipIndex[val])
+    last_val = val
