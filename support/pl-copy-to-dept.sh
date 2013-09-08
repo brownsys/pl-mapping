f6eb0bb6529b2010fa7bad430a301062a4dad651
Andrew Ferguson <adf@cs.brown.edu>
2013-09-07 22:04:39 -0700

    Automatically release each week's data

--- support/pl-copy-to-dept.sh
+++ support/pl-copy-to-dept.sh
@@ -19,3 +19,31 @@ scp -p -q -r "$archive_src/$next_archive" .
 cd $iffinder_dst
 
 scp -p -q "$iffinder_src/$next_archive-*" .
+
+###
+#
+# Add each week to the released data sets
+#
+###
+
+i=$next_archive
+
+cd /research/cogent_map/data_release/
+
+cp -a $archive_dst/$i .
+
+zpaq a $i-raw.zpaq $i/rev-*
+rm $i/rev-*
+zpaq a $i-processed.zpaq $i/
+rm -rf $i
+
+mv $i-processed.zpaq processed/$i.zpaq
+mv $i-raw.zpaq raw/$i.zpaq
+
+openssl sha1 processed/$i.zpaq >> processed.sha1.txt
+openssl sha1 raw/$i.zpaq >> raw.sha1.txt
+
+scp processed/$i.zpaq systems:/vol/web/html/cogent/processed/
+scp raw/$i.zpaq systems:/vol/web/html/cogent/raw/
+scp processed.sha1.txt systems:/vol/web/html/cogent/
+scp raw.sha1.txt systems:/vol/web/html/cogent/
