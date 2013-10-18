3a2907b80cf0d334e32410562fe5a276d34083b3
Rodrigo Fonseca <rfonseca@cs.brown.edu>
2013-10-18 13:48:32 -0400

    Change in path to process.sh

--- plots/process.sh
+++ plots/process.sh
@@ -1,6 +1,6 @@
 export PL_DATA=/research/cogent_map/pl_archives
 export IFFINDER=/research/cogent_map/iffinder-analysis
-export PL_BIN=$HOME/projects/pl-mapping/analysis-scripts
+export PL_BIN=$HOME/work/projects/pl-mapping/analysis-scripts
 
 $PL_BIN/generateInterfaceBreakdown.py $PL_DATA --allWeeks > iface_breakdown.allweeks.dat
 $PL_BIN/generateInterfaceBreakdown.py $PL_DATA --physical --allWeeks > iface_breakdown.allweeks.physical.dat
